b8dfda3e2eb13c9247af9d7b5d11328ac004a362
suchengen <suchengen@gmail.com>
2016-03-29 00:25:11 +0800

    Second

---
+++ src/com/pepleohealth/activity/MyFavoriteActivity.java
@@ -0,0 +1,7 @@
+package com.pepleohealth.activity;
+
+import android.app.Activity;
+
+public class MyFavoriteActivity extends Activity {
+
+}
---
+++ src/com/pepleohealth/activity/ProductDetailActivity.java
@@ -0,0 +1,7 @@
+package com.pepleohealth.activity;
+
+import android.app.Activity;
+
+public class ProductDetailActivity extends Activity {
+
+}
